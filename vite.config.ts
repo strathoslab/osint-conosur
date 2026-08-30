bf1a1eeae57a857c53afa0444ab8ca6464826f75
Strathos Lab <radar@strathoslab.com>
2026-08-30 14:46:02 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/osint-conosur/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
